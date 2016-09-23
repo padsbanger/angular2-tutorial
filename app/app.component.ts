f5e2504b23aa168d409489291895ea9f6c680da1
Michał Lach <kontakt@michal-lach.pl>
2016-09-23 23:12:07 +0200

    hello world

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular App !!! {{title}}</h1>',
+})
+export class AppComponent {
+  title = 'some title'
+}
