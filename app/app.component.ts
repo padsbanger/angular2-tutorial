686afdc9d449cca89d49cace9885665328ebeb60
Michał Lach <kontakt@michal-lach.pl>
2017-04-17 12:42:31 +0200

    fresh

--- app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular App !!! {{title}}</h1>',
-})
-export class AppComponent {
-  title = 'some title'
-}
